1bc4d24016c581a341dab07b7bd54b41775b149c
Samuel Howell <rugnirtheviking@gmail.com>
2023-12-17 08:45:27 +0100

    added death particles

--- enemy.gd
+++ enemy.gd
@@ -8,7 +8,7 @@ var target: Vector2
 var nav_velocity: Vector2
 @export var hp_max: int
 var hp_current: int
-
+var ded: bool = false
 func _ready():
 	if drill:
 		set_target(drill.global_position)
@@ -28,33 +28,52 @@ func set_target(new_target: Vector2):
 func remaining_distance():
 	return navigation.distance_to_target()
 	
+func _deth() -> void:
+	queue_free()
 func hit(body):
-	if body.is_in_group("bullet"):
-		hp_current -= body.damage
-		$TextureProgressBar.value = hp_current
-		$TextureProgressBar.visible = true
-		if hp_current <= 0:
-			queue_free()
+	if not ded:
+		if body.is_in_group("bullet"):
+			hp_current -= body.damage
+			$TextureProgressBar.value = hp_current
+			$TextureProgressBar.visible = true
+			if hp_current <= 0:
+				$Sprite2D.visible=false
+				$DeathParticles.visible=true
+				$DeathParticles.emitting=true
+				$DeathParticles.restart()
+				var deathTimer = Timer.new()
+				deathTimer.set_wait_time(2)
+				deathTimer.set_one_shot(true)
+				deathTimer.connect("timeout", _deth)
+				# remove self from the enemies group
+				remove_from_group("enemies")
+				# hide the health bar
+				$TextureProgressBar.visible = false
+				add_child(deathTimer)
+				deathTimer.start()
+				ded = true
+			
+			
 
 func _physics_process(delta):
-	
-	if not navigation.is_navigation_finished():
-		var movement_delta = speed * delta
-		var current_agent_position = global_position
-		var next_path_position = navigation.get_next_path_position()
+	if not ded:
+		if not navigation.is_navigation_finished():
+			var movement_delta = speed * delta
+			var current_agent_position = global_position
+			var next_path_position = navigation.get_next_path_position()
+			
+			$Line2D.points = navigation.get_current_navigation_path()
+			var intended_velocity = (next_path_position - current_agent_position).normalized() * movement_delta
+			velocity = intended_velocity
+			
+			move_and_slide()
+			#navigation.set_velocity(intended_velocity)
 		
-		$Line2D.points = navigation.get_current_navigation_path()
-		var intended_velocity = (next_path_position - current_agent_position).normalized() * movement_delta
-		velocity = intended_velocity
-		
-		move_and_slide()
-		#navigation.set_velocity(intended_velocity)
-	
-	# calc distance to target
-	var distance_to_target = (target - global_position).length()
-	if distance_to_target < 30:
-		print("target reached")
-		queue_free()
+		# calc distance to target
+		var distance_to_target = (target - global_position).length()
+		if distance_to_target < 30:
+			print("target reached")
+			queue_free()
 
 
 
